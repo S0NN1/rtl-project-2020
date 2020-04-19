240c3bf634a9dc4f54dfc1acec3301c918bace34
Nicolò Sonnino <swagpino@gmail.com>
2020-04-19 12:52:22 +0200

    backup

--- src/rtl-project.srcs/sources_1/new/10569180.vhd
+++ src/rtl-project.srcs/sources_1/new/10569180.vhd
@@ -1,55 +1,78 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- company: 
+-- engineer: 
 -- 
--- Create Date: 04/17/2020 05:05:22 PM
--- Design Name: 
--- Module Name: 10569180 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- create date: 04/17/2020 05:05:22 pm
+-- design name: 
+-- module name: 10569180 - behavioral
+-- project name: 
+-- target devices: 
+-- tool versions: 
+-- description: 
 -- 
--- Dependencies: 
+-- dependencies: 
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- revision:
+-- revision 0.01 - file created
+-- additional comments:
 -- 
 ----------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+-- uncomment the following library declaration if using
+-- arithmetic functions with signed or unsigned values
+--use ieee.numeric_std.all;
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- uncomment the following library declaration if instantiating
+-- any xilinx leaf cells in this code.
+--library unisim;
+--use unisim.vcomponents.all;
 
 entity project_reti_logiche is
-    port (
-        i_clk: in  std_logic;
-        i_start: in  std_logic;
-        i_rst: in  std_logic;
-        i_data: in  std_logic_vector(7 downto 0);
-        o_address: out std_logic_vector(15 downto 0);
-        o_done: out std_logic;
-        o_en: out std_logic;
-        o_we: out std_logic;
-        o_data: out std_logic_vector (7 downto 0)
-        );
-    end project_reti_logiche;
+	port(
+		i_clk     : in  std_logic;      --clock synchronizing FSM
+		i_start   : in  std_logic;      --signal to start read RAM process
+		i_rst     : in  std_logic;      --signal resetting FSM
+		i_data    : in  std_logic_vector(7 downto 0); --signal from RAM with value requested
+		o_address : out std_logic_vector(15 downto 0); --signal from FSM to RAM requesting address
+		o_done    : out std_logic;      --signal identifying session terminated
+		o_en      : out std_logic;      --signal to access RAM
+		o_we      : out std_logic;      --signal to write on RAM
+		o_data    : out std_logic_vector(7 downto 0) --signal containing data to write
+	);
+end project_reti_logiche;
 
-
-architecture Behavioral of 10569180 is
+architecture behavioral of project_reti_logiche is
+	type state_type is (reset, start, first_read, read_ram, fetch_ram);
+	signal current_state, next_state : state_type; --FSM states
+	signal value                     : std_logic_vector(7 downto 0); --8th address containing input value 
 
 begin
-
-
-end Behavioral;
-
+	process(i_clk)
+	begin
+		if rising_edge(i_clk) then
+			if i_rst = '1' then
+				current_state <= reset;
+			end if;
+		else
+			current_state <= next_state;
+		end if;
+	end process;
+	process(current_state)
+	begin
+		case current_state is
+			when reset =>
+				o_en   <= '0';
+				o_we   <= '0';
+				o_done <= '0';
+			when start =>
+				if i_start = '1' then
+					o_en <= '1';
+				end if;
+			when first_read =>
+			when read_ram =>
+			when fetch_ram =>
+		end case;
+	end process;
+end behavioral;
